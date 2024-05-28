fe248200f279acd86d63d773336c3f05a6d64663
Egan Schafer <eganschafer@gmail.com>
2024-05-28 13:31:41 -0600

    Added home feed generation from dummy database

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 
-app.get('/', (req, res) => res.send('<h1>Hello Express</h1>'));
+app.get('/', (req, res) => require("./src/controllers/homeFeed").getHomeWithFeed(res));
 app.get('/public/:target', (req, res) => res.sendFile("./public/" + req.params.target, {root: __dirname }));
-app.get('/handle', (req, res) => res.render('example.handlebars', { doesWhat: "test"}));
 
 app.listen(PORT, console.log('Server is running on port: ' + PORT));
