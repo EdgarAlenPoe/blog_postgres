e117ac403f514aaf534258fa2d7dc92a0bf7a399
Egan Schafer <eganschafer@gmail.com>
2024-05-30 08:20:30 -0600

    Added front end framework for displaying posts

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 
-app.get('/', (req, res) => require("./src/controllers/homeFeed").getHomeWithFeed(res));
+app.get('/', (req, res) => require("./src/controllers/homeFeed").getHomeWithFeed(req, res));
+app.get('/posts/:target', (req, res) => require("./src/controllers/post").getPost(req, res));
+app.get('/about', (req, res) => {
+	req.target = "about";
+	require("./src/controllers/post").getPost(req, res);
+});
+// posts page
+// tags page
 app.get('/public/:target', (req, res) => res.sendFile("./public/" + req.params.target, {root: __dirname }));
 
 app.listen(PORT, console.log('Server is running on port: ' + PORT));
--- src/controllers/homeFeed.js
+++ src/controllers/homeFeed.js
@@ -1,14 +1,15 @@
-function getHomeWithFeed(res) {
+function getHomeWithFeed(req, res) {
 	// Get data from database
 	// TODO: actually do that
 
 	// temp dummy database data
-	data = [{ title: "Test", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu nibh nulla. Nullam et pretium risus. Suspendisse id ultrices nisl, in imperdiet diam. Suspendisse eu nibh nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi ipsum orci, euismod vel auctor nec, imperdiet ut ante. Nullam vestibulum, orci et commodo euismod, tortor urna faucibus sapien, sit amet lobortis libero risus non urna. Cras sit amet pharetra nibh, blandit tincidunt odio. Cras cursus, lacus in rutrum venenatis, arcu diam laoreet lorem, at aliquam purus odio ut dui. In dapibus, est quis mattis ultrices, sapien dolor congue turpis, at commodo dui ex in risus. Vivamus pellentesque ipsum nulla, vel imperdiet nisi laoreet id. Maecenas blandit eu velit ac sodales. Curabitur nec odio velit. Cras auctor purus at mi condimentum cursus. Pellentesque quis ultricies ligula, in gravida nunc. ", link: ""}]
+	data = [{ title: "Test", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu nibh nulla. Nullam et pretium risus. Suspendisse id ultrices nisl, in imperdiet diam. Suspendisse eu nibh nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi ipsum orci, euismod vel auctor nec, imperdiet ut ante. Nullam vestibulum, orci et commodo euismod, tortor urna faucibus sapien, sit amet lobortis libero risus non urna. Cras sit amet pharetra nibh, blandit tincidunt odio. Cras cursus, lacus in rutrum venenatis, arcu diam laoreet lorem, at aliquam purus odio ut dui. In dapibus, est quis mattis ultrices, sapien dolor congue turpis, at commodo dui ex in risus. Vivamus pellentesque ipsum nulla, vel imperdiet nisi laoreet id. Maecenas blandit eu velit ac sodales. Curabitur nec odio velit. Cras auctor purus at mi condimentum cursus. Pellentesque quis ultricies ligula, in gravida nunc. ", link: "firstPost"}]
+	data.push(data[0]);
 	// add entry for each database entry, up to a max of 10
 	output = { feed: []}
 	for (const item of data) {
 		console.log(item.title);
-		outputItem = { title: item.title, content: item.content.slice(0, 300), link: data.link};
+		outputItem = { title: item.title, content: item.content.slice(0, 300), link: item.link};
 		output.feed.push(outputItem);
 	}
 	// render
